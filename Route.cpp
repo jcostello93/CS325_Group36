841e6c39c839f4c5f4260b6703dad6b55353e9f5
jcostello93 <costejoh@oregonstate.edu>
2018-03-13 20:37:49 +0100

    Update Route.cpp
    Included edges

--- Route.cpp
+++ Route.cpp
@@ -60,6 +60,10 @@ int Route::calculateDistance() {
 
 		if (current->getNeighbors()[idx]->getColor() == "white") {
 			Q.push(current->getNeighbors()[idx]);
+			Edge* e = new Edge(current, current->getNeighbors()[idx], current->getX(), current->getNeighbors()[idx]->getX(), current->getY(), current->getNeighbors()[idx]->getY()); 
+			current->setEdgeOut(e);
+			current->getNeighbors()[idx]->setEdgeIn(e); 
+			edges.push_back(e); 
 		}
 
 	}
@@ -91,6 +95,11 @@ int Route::goBackHome(City* current) {
 	sqr = sqrt(sum);
 
 	route.push_back(cities[0]); 
+
+	Edge* e = new Edge(current, cities[0], current->getX(), cities[0]->getX(), current->getY(), cities[0]->getY());
+	current->setEdgeOut(e);
+	cities[0]->setEdgeIn(e);
+	edges.push_back(e);
 	cout << "From " << current->getId() << " to " << cities[0]->getId() << endl; 
 
 	return sqr; 
@@ -101,4 +110,73 @@ void Route::printRoute() {
 		cout << i << ": " << route[i]->getId() << endl; 
 	}
 
+	cout << "Edges: " << endl; 
+
+	for (int i = 0; i < edges.size(); i++) {
+		cout << "Edge " << i << "    Beginning City: " << edges[i]->getBegEdge()->getId() << "   "; 
+		cout << "End City: " << edges[i]->getEndEdge()->getId() << endl; 
+	}
+
+}
+
+void Route::checkEdges() {
+
+	for (int i = 0; i < edges.size(); i++) {
+		Edge *e1 = edges[i];
+		for (int j = i + 1; j < edges.size(); j++) {
+			Edge *e2 = edges[j]; 
+
+			int a = checkIntersect(e1, e2); 
+
+			if (a == 1) {
+				cout << "Swap!" << endl; 
+			} 
+			else {
+				cout << "No swap!" << endl; 
+			}
+		}
+	}
+}
+
+int Route::checkIntersect(Edge* e1, Edge* e2) {
+	int e1_x1 = e1->getBegEdge()->getX();
+	int e1_y1 = e1->getBegEdge()->getY();
+	int e1_x2 = e1->getEndEdge()->getX();
+	int e1_y2 = e1->getEndEdge()->getY();
+	int e2_x1 = e2->getBegEdge()->getX();
+	int e2_y1 = e2->getBegEdge()->getY();
+	int e2_x2 = e2->getEndEdge()->getX();
+	int e2_y2 = e2->getEndEdge()->getY();
+
+	int o1 = orientation(e1_x1, e1_y1, e1_x2, e1_y2, e2_x1, e2_y1); 
+	int o2 = orientation(e1_x1, e1_y1, e1_x2, e1_y2, e2_x2, e2_y2);
+	int o3 = orientation(e2_x1, e2_y1, e2_x2, e2_y2, e1_x1, e1_y1); 
+	int o4 = orientation(e2_x1, e2_y1, e2_x2, e2_y2, e1_x2, e1_y2);
+
+	cout << "e1_x1: " << e1_x1 << "  e1_y1: " << e1_y1 << endl;
+	cout << "e1_x2: " << e1_x2 << "  e1_y2: " << e1_y2 << endl;
+	cout << "e2_x1: " << e2_x1 << "  e2_y1: " << e2_y1 << endl;
+	cout << "e2_x2: " << e2_x2 << "  e2_y2: " << e2_y2 << endl;
+
+	if (e1_x2 != e2_x1 && e1_y2 != e2_y1) {
+		if (o1 != o2 && o3 != o4) {
+			return 1;
+		}
+	}
+
+	return 0; 
+
+
+}
+
+int Route::orientation(int p_x, int p_y, int q_x, int q_y, int r_x, int r_y) {
+
+	int val = (q_y - p_y) * (r_x - q_x) - (q_x - p_x) * (r_y - q_y);
+
+	if (val > 0) {
+		return 1;
+	}
+	else {
+		return 0; 
+	}
 }
